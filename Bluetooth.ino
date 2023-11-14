81198f2ef27e0521e2ec3175162de613baa841ce
Xxproner <pavelchessbur@mail.ru>
2023-11-14 12:20:44 +0300

    ad converter for termo device

--- Bluetooth.ino
+++ Bluetooth.ino
@@ -11,21 +11,39 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   BTserial.begin(9600);
+  pinMode(2, OUTPUT); //red
+  pinMode(3, OUTPUT); //yellow
+  pinMode(4, OUTPUT); //green
+}
 
+double fixtemp = 20.0;
+
+double Thermistor(int RawADC) // convert voltage to temperature celsius
+{
+  Serial.println(RawADC);
+  return 20.0 - (RawADC - 560) / 5.7;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  while (Serial.available()) {}
+  while (BTserial.available() == 0) {} // while nothing to read
   String command = BTserial.readString();
   command.trim();
   if (command == "temp"){
-    Serial.print(analogRead(temp_pin));
+    digitalWrite(3, LOW); // traffic light
+    digitalWrite(4, LOW);
+    digitalWrite(2, HIGH);
+    Serial.print("The space temperature = "); 
+    Serial.println(Thermistor(analogRead(temp_pin)));
   } else if (command == "dist"){
-    digitalWrite(dist_power, HIGH);
-    Serial.print(analogRead(dist_pin));
-    digitalWrite(dist_power, LOW);
+    digitalWrite(2, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(3, HIGH);
+    Serial.print("The distance ... = "); 
+    Serial.println(analogRead(dist_pin)); 
   } else{
-    // nothing
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, HIGH);
   }
 }
