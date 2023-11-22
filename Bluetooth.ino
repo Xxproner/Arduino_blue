a5f9d85b2313658f70b81a639d706b3c915ff9bc
OdincovMD <150460572+OdincovMD@users.noreply.github.com>
2023-11-22 20:57:17 +0300

    Update Bluetooth.ino

--- Bluetooth.ino
+++ Bluetooth.ino
@@ -1,97 +1,107 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial BTserial(8, 9); // rxd and txd
+SoftwareSerial BTserial(8, 9);
 
-const int temp_pin = 0;
-const int dist_pin = 1;
+const int temp_pin = A0;
+const int dist_pin = A1;
+const int signal_pin = A2;
+const int noise_pin = A3; 
 const int dist_power = 12;
-const int signal_pin = 5;
-const int lees = A0; 
-int detect = 500; // значение, которое датчик выдаёт при засечение предмета равно порядка 200
+const int presence_power = 11;
+const int noise_power = 10;
+
+int detect = 500;
 int counter = 1; 
-// пин 9 занят пищалкой //
 
 void setup() {
-  pinMode(lees, INPUT);
-  pinMode(temp_pin, OUTPUT);
-  pinMode(signal_pin, INPUT); // пин для датчика присутсвия
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  BTserial.begin(9600);
+  pinMode(dist_pin, INPUT);
+  pinMode(temp_pin, INPUT);
+  pinMode(signal_pin, INPUT);
+  pinMode(dist_power, OUTPUT);
+  pinMode(presence_power, OUTPUT);
+  pinMode(noise_power, OUTPUT);
   pinMode(2, OUTPUT); //red
   pinMode(3, OUTPUT); //yellow
-  pinMode(4, OUTPUT); //green
+  pinMode(4, OUTPUT); //green 
+  pinMode(7, OUTPUT);
+  Serial.begin(9600);
+  BTserial.begin(9600);
 }
 
 double fixtemp = 20.0;
 
-double Thermistor(int RawADC) // convert voltage to temperature celsius
-{
-  Serial.println(RawADC);
+double Thermistor(int RawADC) {
   return 20.0 - (RawADC - 560) / 5.7;
 }
 
+void traffic_light(int i, int j, int k) {
+  digitalWrite(i, LOW);
+  digitalWrite(j, LOW);
+  digitalWrite(k, HIGH);
+}
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  while (BTserial.available() == 0) {} // while nothing to read
+  while (BTserial.available() == 0) {}
   String command = BTserial.readString();
   command.trim();
-  if (command == "temp"){
-    digitalWrite(3, LOW); // traffic light
-    digitalWrite(4, LOW);
-    digitalWrite(2, HIGH);
-    Serial.print("The space temperature = "); 
-    Serial.println(Thermistor(analogRead(temp_pin)));
-  } else if (command == "dist"){
-      if (analogRead(lees) < detect) {
-        Serial.print("Объект обнаружен. Количесвто обнаружений = ");
-        Serial.println(counter);
-        counter += 1;
-        digitalWrite(4, LOW);
-        digitalWrite(3, LOW);
-        digitalWrite(2, HIGH);
-        delay(8000);
-      } else {
-        digitalWrite(2, LOW);
-        digitalWrite(3, LOW);
-        digitalWrite(4, HIGH);
+  if (command == "temp") {
+    while (command != "no") {
+      traffic_light(4, 3, 2);
+      BTserial.print("The space temperature = "); 
+      BTserial.println(Thermistor(analogRead(temp_pin)));
+      delay(2000);
+      command = BTserial.readString();
+    }
+  } else if (command == "dist") {
+      while (command != "no") {
+          digitalWrite(dist_power, HIGH);
+          if (analogRead(dist_pin) < detect) {
+            BTserial.print("Object has been detected. A number of detections = ");
+            BTserial.println(counter);
+            counter += 1;
+            traffic_light(4, 3, 2);
+            delay(8000);
+          } else {
+            traffic_light(2, 3, 4);
+          }
+          command = BTserial.readString();
       }
-  } else if (command == 'presence'){ // датчик присутсвия 
-      if (digitalRead(signal_pin) == HIGH) {
-        digitalWrite(4, LOW); // горит красный, если что-то есть 
-        digitalWrite(3, LOW);
-        digitalWrite(2, HIGH); 
-        Serial.println("Обнаружен."); // можно вывести на телефон
-        tone(9, 2000); // пищим, если что-то есть
-      } else {
-        digitalWrite(2, LOW);
-        digitalWrite(3, LOW);
-        digitalWrite(4, HIGH);
-        Serial.println("Не обнаружен."); 
-        noTone(9);
+  } else if (command == "presence") { // датчик присутсвия 
+      while (command != "no") {
+        digitalWrite(presence_power, HIGH);
+        if (digitalRead(signal_pin) == HIGH) {
+          traffic_light(4, 3, 2);
+          BTserial.println("Detected.");
+          tone(7, 2000);
+        } else {
+          traffic_light(2, 3, 4);
+          BTserial.println("No detected.");  
+          noTone(7);
+        }
+        command = BTserial.readString();
+        delay(1000);
       }
-      delay(1000);
-  } else if (command == 'noise') {
-      Serial.print("Уровень шума : ");
-      Serial.println(analogRead(lees));
-      delay(500);
-      if ((analogRead(analog))  < 40 ) { // порог срабатывания зелёного светодиода, мВ по дефолту в тишине меньше 40
-        digitalWrite(4, HIGH);
-        digitalWrite(3, LOW);
-        digitalWrite(2, LOW);
-      } else if ((analogRead(analog)) < 300) { // порог срабатывания жёлтого светодиода, мВ
-        digitalWrite(4, LOW);
-        digitalWrite(3, HIGH);
-        digitalWrite(2, LOW);
-      } else { // порог срабатывания красного светодиода, мВ
-        digitalWrite(4, LOW);
-        digitalWrite(3, LOW);
-        digitalWrite(2, HIGH);    
+      noTone(7);
+  } else if (command == "noise") {
+      while (command != "no") {
+        digitalWrite(noise_pin, HIGH);
+        BTserial.print("Level noises : ");
+        int read = 0;
+        read = analogRead(noise_pin);
+        BTserial.println(read);
+        delay(500);
+        if (read  < 40 ) { 
+          traffic_light(2, 3, 4);
+        } else if (read < 300) { 
+          traffic_light(2, 4, 3);
+          
+        } else {
+          traffic_light(4, 3, 2); 
+        }
+        command = BTserial.readString();
       }
   }
-  else{
-    digitalWrite(2, LOW);
-    digitalWrite(3, LOW);
-    digitalWrite(4, HIGH);
+  else {
+    traffic_light(2, 3, 4);
   }
 }
